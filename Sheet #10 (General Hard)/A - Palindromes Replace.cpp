f7ccc70e4efa447c437ab53a5e0018b8b947e859
MOAZKING1001 <101853858+MOAZKING1001@users.noreply.github.com>
2024-01-23 20:52:57 +0200

    A - Palindromes Replace

--- Sheet #10 (General Hard)/A - Palindromes Replace.cpp
+++ Sheet #10 (General Hard)/A - Palindromes Replace.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <array>
 #include <string>
-#include <algorithm>
 using namespace std;
 string algorithm(string text) {
 	for (int i = 0; i < text.length(); i++) {
