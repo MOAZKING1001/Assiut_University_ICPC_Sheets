7b220ce956bc14e4d62ef14453efb7a20612ef35
MOAZKING1001 <101853858+MOAZKING1001@users.noreply.github.com>
2024-01-24 02:47:11 +0200

    F - Marks

--- Sheet #10 (General Hard)/F - Marks.cpp
+++ Sheet #10 (General Hard)/F - Marks.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <array>
-#include <string>
-#include <algorithm>
 using namespace std;
 int algorithm(int students, int subjects) {
 	vector<int> marks;
