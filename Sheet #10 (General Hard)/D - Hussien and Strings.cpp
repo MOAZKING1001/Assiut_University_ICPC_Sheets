75c1781e092487f5a9ca2b13c3ce3dd339b6725d
MOAZKING1001 <101853858+MOAZKING1001@users.noreply.github.com>
2024-01-24 03:24:52 +0200

    D - Hussien and Strings

--- Sheet #10 (General Hard)/D - Hussien and Strings.cpp
+++ Sheet #10 (General Hard)/D - Hussien and Strings.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
-string algorithm(string str1, string str2) {
+bool algorithm(string str1, string str2) {
 	string original2 = str2;
-	if (str1 == str2) return "True";
+	if (str1 == str2) return true;
 	for (int i = 0, j = 0; i < str1.length(); i++, j++) {
 		for (int j = 0; j < str1.length(); j++) {
 			swap(str1[i], str1[j]);
-			if (str1 == str2) return "True";
+			if (str1 == str2) return true;
 			str2 = original2;
 		}
 	}
-	return "False";
+	return false;
 }
 int main() {
-	cout << algorithm("hello", "heoll");
+	string str1, str2;
+	cin >> str1;
+	cin >> str2;
+	cout << algorithm(str1, str2);
 }
