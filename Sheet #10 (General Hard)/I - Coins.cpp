933e6abe2743caf845834d47398dae3d8821d53c
MOAZKING1001 <101853858+MOAZKING1001@users.noreply.github.com>
2024-01-24 02:49:38 +0200

    I - Coins

--- Sheet #10 (General Hard)/I - Coins.cpp
+++ Sheet #10 (General Hard)/I - Coins.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <array>
 #include <string>
-#include <algorithm>
 using namespace std;
 string algorithm() {
 	vector<string> expresions;
