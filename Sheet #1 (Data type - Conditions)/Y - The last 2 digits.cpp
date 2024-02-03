001667034726657843e4625c365a703bf10b4c9c
MOAZ KING <101853858+MOAZKING1001@users.noreply.github.com>
2024-02-03 02:23:23 +0200

    Y - The last 2 digits

--- Sheet #1 (Data type - Conditions)/Y - The last 2 digits.cpp
+++ Sheet #1 (Data type - Conditions)/Y - The last 2 digits.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main() {
-	unsigned long long n1, n2, n3, n4;
+	long long n1, n2, n3, n4;
 	cin >> n1 >> n2 >> n3 >> n4;
-	string temp = to_string(n1 * n2 * n3 * n4);
-	cout << *(temp.end() - 2) << *(temp.end() - 1);
+	long long res = n1 % 100 * n2 % 100 * n3 % 100 * n4 % 100;
+	if (res % 100 < 10) cout << "0" << res % 100;
+	else cout << res % 100;
 }
