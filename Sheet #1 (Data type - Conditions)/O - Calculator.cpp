bfafbc1f0e7d7846662e456d3099057410ba671f
MOAZ KING <101853858+MOAZKING1001@users.noreply.github.com>
2024-02-03 02:51:47 +0200

    O - Calculator

--- Sheet #1 (Data type - Conditions)/O - Calculator.cpp
+++ Sheet #1 (Data type - Conditions)/O - Calculator.cpp
@@ -3,37 +3,29 @@
 using namespace std;
 int main() {
 	string input;
-	string temp = "";
-	char operation;
-	int nums[2];
-	getline(cin, input);
-	for (int i = 0, j = 0; i < input.length(); i++) {
-		if (input[i] != '+' && input[i] != '-' && input[i] != '*' && input[i] != '/' && i != input.length() - 1) {
-			temp += input[i];
-		}
-		else {
-			if (i == input.length() - 1) {
-				temp += input[i];
-			}
-			else {
-				operation = input[i];
-			}
-			nums[j] = stoi(temp);
-			j++;
-			temp = "";
+	cin >> input;
+	int num1, num2, opIndex;
+	char op;
+	for (int i = 0; i < input.length(); i++) {
+		if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/') {
+			opIndex = i;
+			op = input[i];
+			break;
 		}
 	}
-	switch (operation) {
+	num1 = stoi(string(input.begin(), input.begin() + opIndex));
+	num2 = stoi(string(input.begin() + opIndex + 1, input.end()));
+	switch (op) {
 		case '+':
-			cout << nums[0] + nums[1];
+			cout << num1 + num2;
 			break;
 		case '-':
-			cout << nums[0] - nums[1];
+			cout << num1 - num2;
 			break;
 		case '*':
-			cout << nums[0] * nums[1];
+			cout << num1 * num2;
 			break;
 		case '/':
-			cout << nums[0] / nums[1];
+			cout << num1 / num2;
 	}
 }
