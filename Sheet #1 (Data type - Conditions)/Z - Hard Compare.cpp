e249a4aac991d52ff93db72b1fafeb5c347868e7
MOAZ KING <101853858+MOAZKING1001@users.noreply.github.com>
2024-02-03 02:24:15 +0200

    Z - Hard Compare

--- Sheet #1 (Data type - Conditions)/Z - Hard Compare.cpp
+++ Sheet #1 (Data type - Conditions)/Z - Hard Compare.cpp
@@ -2,8 +2,10 @@
 #include <cmath>
 using namespace std;
 int main() {
-	long long num1, num2, num3, num4;
+	double num1, num2, num3, num4;
 	cin >> num1 >> num2 >> num3 >> num4;
-	if (pow(num1, num2) > pow(num3, num4)) cout << "YES";
+	double res1 = num2 * log(num1);
+	double res2 = num4 * log(num3);
+	if (res1 > res2) cout << "YES";
 	else cout << "NO";
 }
