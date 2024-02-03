14d4c2710de16490e3cb8e863e05799781ccbfc5
MOAZ KING <101853858+MOAZKING1001@users.noreply.github.com>
2024-02-03 05:29:06 +0200

    D - Ali Baba and Puzzles

--- Contest #1/D - Ali Baba and Puzzles.cpp
+++ Contest #1/D - Ali Baba and Puzzles.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-	
+	long long a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (a + b - c == d || a + b * c == d ||
+		a - b + c == d || a - b * c == d ||
+		a * b + c == d || a * b - c == d
+		) cout << "YES";
+	else cout << "NO";
 }
