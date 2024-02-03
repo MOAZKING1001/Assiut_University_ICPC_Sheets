c3bac56a05a80dc520f0dc2e244b8edad4a5e054
MOAZ KING <101853858+MOAZKING1001@users.noreply.github.com>
2024-02-03 20:25:32 +0200

    F - Marks

--- Sheet #10 (General Hard)/F - Marks.cpp
+++ Sheet #10 (General Hard)/F - Marks.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
-int algorithm() {
-	int students, subjects;
-	string temp;
-	getline(cin, temp);
-	students = temp[0] - '0';
-	subjects = temp[2] - '0';
-	vector<int> marks;
-	vector<int> max_marks;
+int main() {
+	long long students, subjects;
+	cin >> students >> subjects;
+	vector<long long> marks;
+	vector<long long> max_marks;
 	string marks_per_student_holder;
-	int students_passed = 0;
-	for (int i = 0; i < students; i++) {
+	long long students_passed = 0;
+	for (long long i = 1; i <= students; i++) {
 		cin >> marks_per_student_holder;
-		for (int j = 0; j < subjects; j++) {
+		for (long long j = 0; j < subjects; j++) {
 			marks.push_back(marks_per_student_holder[j] - '0');
 		}
 	}
-	for (int i = 0; i < subjects; i++) {
-		int max_temp = 0;
-		for (int j = i, k = 0; k < students; j += subjects, k++) {
-			if (marks.at(j) > max_temp) max_temp = marks.at(j);
+	for (long long i = 0; i < subjects; i++) {
+		int temp_max = 0;
+		for (long long j = i, k = 0; k < students; j += subjects, k++) {
+			if (marks.at(j) > temp_max) temp_max = marks.at(j);
 		}
-		max_marks.push_back(max_temp);
+		max_marks.push_back(temp_max);
 	}
-	for (int i = 0, j = 0; i < students; i++, j += subjects) {
+	for (long long i = 0, j = 0; i < students; i++, j += subjects) {
 		int passed_temp = 0;
-		for (int k = 0, l = j; k < subjects; k++, l++) {
+		for (long long k = 0, l = j; k < subjects; k++, l++) {
 			if (marks.at(l) == max_marks.at(k)) passed_temp++;
 		}
 		if (passed_temp) students_passed++;
 	}
-	return students_passed;
-}
-int main() {
-	cout << algorithm();
+	cout << students_passed;
 }
