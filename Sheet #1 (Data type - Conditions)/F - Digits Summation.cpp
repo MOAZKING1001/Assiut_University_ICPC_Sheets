32521d285b48eb455693437d7232f25ca9dc17f6
MOAZ KING <101853858+MOAZKING1001@users.noreply.github.com>
2024-02-07 07:21:42 +0200

    F - Digits Summation

--- Sheet #1 (Data type - Conditions)/F - Digits Summation.cpp
+++ Sheet #1 (Data type - Conditions)/F - Digits Summation.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
-	long long num, sum = 0;
-	cin >> num;
-	sum = num * (num + 1) / 2;
-	cout << sum;
+	string num1, num2;
+	cin >> num1 >> num2;
+	cout << num1.back() - '0' + num2.back() - '0';
 }
